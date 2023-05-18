3bdecdc92c0fb736682e5ee040b5d23cbb5ff79d
Eekshit Chawla <chawlaeekshit65@gmail.com>
2023-05-19 02:23:00 +0530

    team

--- src/screens/Team/Team.js
+++ src/screens/Team/Team.js
@@ -1,64 +1,25 @@
 // import React from "react";
-// import '../Team/Team.css';
-// import avatar from '../../assets/avatar.png';
-
-// import Slider from "react-slick";
-
-// const Team = () => {
-
-//     const members = [
-//         { key: 1, memName: "SreSanjai1", designation: "Owner", picUrl: avatar },
-//         { key: 2, memName: "SreSanjai2", designation: "Manager", picUrl: avatar },
-//         { key: 3, memName: "SreSanjai3", designation: "Developer", picUrl: avatar },
-//         { key: 4, memName: "SreSanjai4", designation: "Owner", picUrl: avatar },
-//         { key: 5, memName: "SreSanjai5", designation: "Manager", picUrl: avatar },
-//         { key: 6, memName: "SreSanjai6", designation: "Developer", picUrl: avatar },
-//         { key: 7, memName: "SreSanjai7", designation: "Developer", picUrl: avatar },
-//     ];
-
-//     const memberCards = members.map((member) => (
-//         <div key={member.key} id="glassCardContainer">
-//             <div id="glassCard">
-//                 <img id="avatar" src={member.picUrl} alt="avatar" />
-//                 <div id="nameInCard"><strong>{member.memName}</strong></div>
-//                 <div id="designationInCard">{member.designation}</div>
-//             </div>
-//         </div>
-//     ));
-
-//     return (
-//         <div id="teamPageCard">
-//             <div id="memberCardsContainer">
-//                 <Slider>
-//                     {/* {memberCards} */}
-//                     {
-//                         members.map((member) => (
-//                             <div key={member.key} id="glassCardContainer">
-//                                 <div id="glassCard">
-//                                     <img id="avatar" src={member.picUrl} alt="avatar" />
-//                                     <div id="nameInCard"><strong>{member.memName}</strong></div>
-//                                     <div id="designationInCard">{member.designation}</div>
-//                                 </div>
-//                             </div>
-//                         ))
-//                     }
-//                 </Slider>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Team;
-
-
-import React from "react";
 import "../Team/Team.css";
 import avatar from "../../assets/avatar.png";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import tri from "../../assets/msgTriangle.png";
+import mail from "../../assets/mailBlack.png";
+import insta from "../../assets/instaPic.png";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import React, { useState } from 'react';
 
 const Team = () => {
+
+    const [isClickedFaq, setClickedFaq] = useState(false);
+    const [isClickedHost, setClickedHost] = useState(false);
+
+    const handleFaq = () => {
+        setClickedFaq(!isClickedFaq);
+    };
+    const handleHost = () => {
+        setClickedHost(!isClickedHost)
+    }
     const members = [
         { key: 1, memName: "SreSanjai1", designation: "Owner", picUrl: avatar },
         { key: 2, memName: "SreSanjai2", designation: "Manager", picUrl: avatar },
@@ -97,6 +58,40 @@ const Team = () => {
                 >
                     {memberCards}
                 </Slider>
+            </div>
+            <div id="footer">
+                <div id="faqContainer">
+                    <div id="faq">FAQ</div>
+                    <div id="dropUps">
+                        <div id="dropUp" onClick={handleFaq}>
+                            {isClickedFaq ? <div id="msgFaq" >Message displayed!</div> : null}
+                            <div id="dropUpContent"> How to use? <img id={isClickedFaq ? "triTurn" : "tri"} src={tri} alt="" /></div>
+                        </div>
+                        <div id="dropUp" onClick={handleHost}>
+                            {isClickedHost ? <span id="msgFaq" >Message displayed!</span> : null}
+                            <div id="dropUpContent"> Host your Property! <img id={isClickedHost ? "triTurn" : "tri"} src={tri} alt="" /></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="reachUs">
+                    <div id="reach">REACH US</div>
+                    <div id="imgsLink">
+                        <div id="mailCont">
+                            <img id="reachImg" src={mail} alt="" />
+                            <u>
+                                <a id="link" href="http://localhost:3000/">Happy to Hear</a>
+                            </u>
+
+                        </div>
+                        <div id="instaCont">
+                            <img id="reachImg" src={insta} alt="" />
+                            <u>
+
+                                <a id="link" href="http://localhost:3000/">Happy to Hear</a>
+                            </u>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/screens/WhyUs/WhyUs.js
+++ src/screens/WhyUs/WhyUs.js
@@ -1,83 +1,3 @@
-import React, { useState } from "react";
-import imgUs from '../../assets/aiml.png';
-import '../WhyUs/WhyUs.css'
-
-const WhyUs = () => {
-    const data = [
-        {
-            id: 1,
-            key: 1,
-            title: "Lorem Ipsum 1",
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-            img: { imgUs }
-        },
-        {
-            id: 2,
-            key: 2,
-            title: "Lorem Ipsum 2",
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-            img: { imgUs }
-        },
-        {
-            id: 3,
-            key: 3,
-            title: "Lorem Ipsum 3",
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-            img: { imgUs }
-        },
-        {
-            id: 4,
-            key: 4,
-            title: "Lorem Ipsum 4",
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-            img: { imgUs }
-        },
-
-    ];
-
-    const [toggle, setToggle] = useState("1");
-
-    return (
-        <div id="whyUsPage">
-            <h1 id="mainHead">What We Offer?</h1>
-            {data.map(({ title, text, key, img }) => {
-                return (
-                    <>
-                        <span className="main">
-                            <h1 className={toggle === key ? "enlarged" : ""} onClick={() => setToggle(key)}>
-                                {title}
-                            </h1>
-
-                            <div className="img">
-                                {
-                                    toggle === key ? (
-                                        <>
-                                            <img alt="" src={imgUs} key={key} className="photo" />
-                                        </>
-                                    ) : null
-                                }
-                            </div>
-                        </span>
-
-                    </>
-                );
-            })}
-            <span id="scrollContainer">
-                {/* {data.map(({ key }) => {
-                    <img alt="" src={imgUs} key={key} className="photoScroll" />
-                })} */}
-                <img alt="" src={imgUs} className="photoScroll" />
-                <img alt="" src={imgUs} className="photoScroll" />
-                <img alt="" src={imgUs} className="photoScroll" />
-                <img alt="" src={imgUs} className="photoScroll" />
-            </span>
-        </div>
-    );
-}
-
-
-export default WhyUs;
-
 // import React, { useState } from "react";
 // import imgUs from '../../assets/aiml.png';
 // import '../WhyUs/WhyUs.css'
@@ -89,64 +9,148 @@ export default WhyUs;
 //             key: 1,
 //             title: "Lorem Ipsum 1",
 //             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-//             img: imgUs
+//             img: { imgUs }
 //         },
 //         {
 //             id: 2,
 //             key: 2,
 //             title: "Lorem Ipsum 2",
 //             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-//             img: imgUs
+//             img: { imgUs }
 //         },
 //         {
 //             id: 3,
 //             key: 3,
 //             title: "Lorem Ipsum 3",
 //             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-//             img: imgUs
+//             img: { imgUs }
 //         },
 //         {
 //             id: 4,
 //             key: 4,
 //             title: "Lorem Ipsum 4",
 //             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
-//             img: imgUs
-//         }
+//             img: { imgUs }
+//         },
+
 //     ];
 
-//     const [activeIndex, setActiveIndex] = useState(0);
-
-//     const handleSlideClick = (index) => {
-//         setActiveIndex(index);
-//     };
+//     const [toggle, setToggle] = useState("1");
 
 //     return (
 //         <div id="whyUsPage">
 //             <h1 id="mainHead">What We Offer?</h1>
-//             <div id="contentContainer">
-//                 <div className="text">
-//                     <h1>{data[activeIndex].title}</h1>
-//                     <p>{data[activeIndex].text}</p>
-//                 </div>
-//                 <div className="slider">
-//                     <div className="slides">
-//                         {data.map(({ title, key, img }, index) => (
-//                             <div
-//                                 key={key}
-//                                 className={`slide ${activeIndex === index ? "active" : ""}`}
-//                                 onClick={() => handleSlideClick(index)}
-//                             >
-//                                 <img src={img} alt="" className="photo" />
-//                             </div>
-//                         ))}
-//                     </div>
-//                 </div>
+//             <div id="bodyWhy">
+//                 <span id="lhs">
+//                     {data.map(({ title, text, key, img }) => {
+//                         return (
+//                             <>
+//                                 <span className="main">
+//                                     <h1 className={toggle === key ? "enlarged" : ""} onClick={() => setToggle(key)}>
+//                                         {title}
+//                                     </h1>
 
+//                                     <div className="img">
+//                                         {
+//                                             toggle === key ? (
+//                                                 <>
+//                                                     <img alt="" src={imgUs} key={key} className="photo" />
+//                                                 </>
+//                                             ) : null
+//                                         }
+//                                     </div>
+//                                 </span>
+
+//                             </>
+//                         );
+//                     })}
+//                 </span>
+//                 <div id="scrollContainer">
+//                     <img alt="" src={imgUs} />
+//                     <img alt="" src={imgUs} />
+//                     <img alt="" src={imgUs} />
+//                     <img alt="" src={imgUs} />
+//                 </div>
+//                 <div id="scrollContainer">
+//                     {data.map(({ id, img }) =>
+//                         toggle == id ? <img alt="" src={img} key={id} /> : null
+//                     )}
+//                 </div>
 //             </div>
 //         </div>
 //     );
 // }
 
+
 // export default WhyUs;
 
+import React, { useState } from "react";
+import imgUs from '../../assets/aiml.png';
+import '../WhyUs/WhyUs.css'
 
+const WhyUs = () => {
+    const data = [
+        {
+            id: 1,
+            key: 1,
+            title: "Lorem Ipsum 1",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
+            img: imgUs
+        },
+        {
+            id: 2,
+            key: 2,
+            title: "Lorem Ipsum 2",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
+            img: imgUs
+        },
+        {
+            id: 3,
+            key: 3,
+            title: "Lorem Ipsum 3",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
+            img: imgUs
+        },
+        {
+            id: 4,
+            key: 4,
+            title: "Lorem Ipsum 4",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequatur reiciendis voluptates, aperiam repellat perferendis qui quidem ipsa rem tempore, eius eos, eligendi saepe? Ipsa illum dicta ipsum officiis numquam.",
+            img: imgUs
+        }
+    ];
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handleSlideClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    return (
+        <div id="whyUsPage">
+            <h1 id="mainHead">What We Offer?</h1>
+            <div id="contentContainer">
+                <div className="text">
+                    <h1>{data[activeIndex].title}</h1>
+                    <p>{data[activeIndex].text}</p>
+                </div>
+                {/* <div className="slider">
+                    <div className="slides">
+                        {data.map(({ title, key, img }, index) => (
+                            <div
+                                key={key}
+                                className={`slide ${activeIndex === index ? "active" : ""}`}
+                                onClick={() => handleSlideClick(index)}
+                            >
+                                <img src={img} alt="" className="photo" />
+                            </div>
+                        ))}
+                    </div>
+                </div> */}
+
+            </div>
+        </div>
+    );
+}
+
+export default WhyUs;
