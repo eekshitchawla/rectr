94372f7822142468297c56577d94591260ade1ea
Eekshit Chawla <chawlaeekshit65@gmail.com>
2023-06-28 19:09:57 +0530

    homepage bg change

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,50 @@
+// import React, { useState } from "react";
+
+// import Features from './screens/Features/Features';
+// import BACKGROUND from './assets/compressedbg.png'
+// import road from './assets/road.png'
+// import WhyUs from './screens/WhyUs/WhyUs';
+// import HowWeWork from './screens/HowWeWork/HowWeWork';
+// import Triangle from './screens/Triangle/Triangle';
+// import Team from './screens/Team/Team';
+// import JoinUs from "./screens/JoinUs/JoinUs";
+
+// const Home = () => {
+
+
+//   const [count, setCount] = useState(0);
+
+
+//   return <>
+//     <div id="landingPage">
+//       <div id="headingContainer">
+//         <div id="heading"><h1 id="headingH1">WELCOME TO RECTR.IN</h1></div>
+//         <div id="content">
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam modi accusamus soluta
+//           sit unde aperiam ipsa suscipit, aspernatur cumque, alias hic ratione, repellat illo voluptate
+//           ipsum asperiores voluptas! Error laborum assumenda nulla totam beatae vel praesentium? Totam
+//           perferendis tempora voluptas eaque quaerat quas in et rem blanditiis facilis? Rem, incidunt.
+//         </div>
+//       </div>
+//       <div id='imgContainer'>
+//         <img id="bgImgg" src={BACKGROUND} alt="background" />
+//       </div>
+//     </div>
+//     <Features />
+//     <WhyUs />
+//     <HowWeWork />
+//     <Triangle setCount={setCount} count={count} />
+//     <JoinUs />
+//     <Team />
+//   </>;
+// };
+
+// export default Home;
+
+
 import React, { useState } from "react";
 
 import Features from './screens/Features/Features';
-import BACKGROUND from './assets/compressedbg.png'
 import WhyUs from './screens/WhyUs/WhyUs';
 import HowWeWork from './screens/HowWeWork/HowWeWork';
 import Triangle from './screens/Triangle/Triangle';
@@ -24,9 +67,6 @@ const Home = () => {
           ipsum asperiores voluptas! Error laborum assumenda nulla totam beatae vel praesentium? Totam
           perferendis tempora voluptas eaque quaerat quas in et rem blanditiis facilis? Rem, incidunt.
         </div>
-      </div>
-      <div id='imgContainer'>
-        <img src={BACKGROUND} alt="background" height="inherit" width="100%" />
       </div>
     </div>
     <Features />
--- src/screens/Navbar/Navbar.js
+++ src/screens/Navbar/Navbar.js
@@ -1,8 +1,98 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import "../Navbar/Navbar.css";
+// import logo from "../../assets/logo.png";
+
+// const Navbar = () => {
+
+//     const colorIt = (val) => {
+//         const textElement = document.getElementById("textToColor");
+//         if (textElement) {
+//             textElement.style.color = "#00B7A9";
+//         }
+//     };
+
+//     useEffect(() => {
+//         let aa = document.body.getBoundingClientRect().top;
+//         const handleScroll = () => {
+//             if (document.body.getBoundingClientRect().top > aa) {
+//                 const navbar = document.getElementById("navbarParentContainer");
+//                 navbar.style.display = "block";
+//                 navbar.style.animation = "bringDown 0.2s 0.2s forwards";
+//             } else {
+//                 const navbar = document.getElementById("navbarParentContainer");
+//                 navbar.style.display = "none";
+//                 navbar.style.animation = "none";
+//             }
+//             aa = document.body.getBoundingClientRect().top;
+//         };
+
+//         window.addEventListener("scroll", handleScroll);
+
+//         return () => {
+//             window.removeEventListener("scroll", handleScroll);
+//         };
+//     }, []);
+
+//     return (
+//         <>
+//             <div id="navbarParentContainer">
+//                 <div id="navbarContainer">
+//                     <div id="logoContainer">
+//                         <div id="logoImgContainer">
+//                             <a href="../#landingPage">
+//                                 <img src={logo} alt="" />
+//                             </a>
+//                         </div>
+//                         <div id="homeTitle">HOME</div>
+//                     </div>
+//                     <div id="listItems">
+//                         <button className="listOfBtns" onClick={() => colorIt('explore')}>
+//                             <a id="navLinks" href="../#landingPage">
+//                                 Explore
+//                             </a>
+//                         </button>
+//                         <button className="listOfBtns" onClick={() => colorIt('features')}>
+//                             <a id="navLinks" href="../#featuresPage">
+//                                 Features
+//                             </a>
+//                         </button>
+//                         <button className="listOfBtns" onClick={() => colorIt('how')}>
+//                             <a id="navLinks" href="../#workPage">
+//                                 How It Works?
+//                             </a>
+//                         </button>
+//                         <button className="listOfBtns" onClick={() => colorIt('why')}>
+//                             <a id="navLinks" href="../#whyUsPage">
+//                                 Why Us?
+//                             </a>
+//                         </button>
+//                     </div>
+//                     <div id="signUpContainer">
+//                         <button type="submit" id="getInButton">
+//                             <a href="../signup">Get In</a>
+//                         </button>
+//                         <div id="theJourneyText">The Journey</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default Navbar;
+
+
+import React, { useEffect, useState } from "react";
 import "../Navbar/Navbar.css";
 import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+    const [underlineColor, setUnderlineColor] = useState("");
+
+    const colorIt = (val) => {
+        setUnderlineColor(val);
+    };
+
     useEffect(() => {
         let aa = document.body.getBoundingClientRect().top;
         const handleScroll = () => {
@@ -38,22 +128,34 @@ const Navbar = () => {
                         <div id="homeTitle">HOME</div>
                     </div>
                     <div id="listItems">
-                        <button className="listOfBtns">
+                        <button
+                            className={`listOfBtns ${underlineColor === "explore" ? "underline" : ""}`}
+                            onClick={() => colorIt("explore")}
+                        >
                             <a id="navLinks" href="../#landingPage">
                                 Explore
                             </a>
                         </button>
-                        <button className="listOfBtns">
+                        <button
+                            className={`listOfBtns ${underlineColor === "features" ? "underline" : ""}`}
+                            onClick={() => colorIt("features")}
+                        >
                             <a id="navLinks" href="../#featuresPage">
                                 Features
                             </a>
                         </button>
-                        <button className="listOfBtns">
+                        <button
+                            className={`listOfBtns ${underlineColor === "how" ? "underline" : ""}`}
+                            onClick={() => colorIt("how")}
+                        >
                             <a id="navLinks" href="../#workPage">
                                 How It Works?
                             </a>
                         </button>
-                        <button className="listOfBtns">
+                        <button
+                            className={`listOfBtns ${underlineColor === "why" ? "underline" : ""}`}
+                            onClick={() => colorIt("why")}
+                        >
                             <a id="navLinks" href="../#whyUsPage">
                                 Why Us?
                             </a>
